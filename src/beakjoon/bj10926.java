bfe8b9e5e8b0cf80ddfab4fa1c20ab201070fa5d
user1-pc <user1-pc@DESKTOP-EBDO9NU>
2023-01-31 16:24:17 +0900

    second

---
+++ src/beakjoon/bj10926.java
@@ -0,0 +1,15 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj10926 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String st = br.readLine();
+		System.out.println(st+"??!");
+	}
+}
--- src/beakjoon/bj18108.java
+++ src/beakjoon/bj18108.java
@@ -1,17 +1,21 @@
 package beakjoon;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class bj2557 {
+public class bj18108 {
 
 	public static void main(String[] args) throws IOException {
-
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String st = "Hello World!";
-		bw.write(st);
+		int num = Integer.parseInt(br.readLine());
+		bw.write(String.valueOf(num-543));
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 }
